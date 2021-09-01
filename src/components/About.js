ffa43233f2cfdc8a2d2da6c5f6e40e1335f22ddf
Lindos-Mahadi <lindosmahadi@.com>
2021-09-01 21:09:28 +0600

    Hide & Show with Toggle

--- src/components/About.js
+++ src/components/About.js
@@ -4,25 +4,20 @@ class About extends Component {
 constructor(){
     super();
     this.state={
-        name : "lindos",
-        email : "lindos@gmail.com",
-        count : 0
+        show:true
     }
 }
-updateState(){
-    this.setState({
-        name: "mahadi",
-        count: this.state.count+1
-    })
-}
+
     render() {
         return (
             <div>
-                <h1>{this.props.text.name} <br /> {this.props.data}</h1>
-                <h1>{this.state.name}</h1>
-                <h1>{this.state.email} </h1>
-                <h1>{this.state.count} </h1>
-                <button onClick={()=>{this.updateState()}}>Update</button>
+                {
+                    this.state.show?
+                        <h1>Hide and Show</h1>
+                    :null
+                }
+                <button onClick={()=>{this.setState({show:false})}}>Update</button>
+                <button onClick={()=>{this.setState({show:!this.state.show})}}>Toggle</button>
             </div>
             )
         }
