18e39b0d47c9256c034397af3731aaf4d8185701
Lindos-Mahadi <lindosmahadi@.com>
2021-09-01 21:01:50 +0600

    Update State with classfull component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class About extends Component {
-
+constructor(){
+    super();
+    this.state={
+        name : "lindos",
+        email : "lindos@gmail.com",
+        count : 0
+    }
+}
+updateState(){
+    this.setState({
+        name: "mahadi",
+        count: this.state.count+1
+    })
+}
     render() {
         return (
             <div>
                 <h1>{this.props.text.name} <br /> {this.props.data}</h1>
+                <h1>{this.state.name}</h1>
+                <h1>{this.state.email} </h1>
+                <h1>{this.state.count} </h1>
+                <button onClick={()=>{this.updateState()}}>Update</button>
             </div>
             )
         }
