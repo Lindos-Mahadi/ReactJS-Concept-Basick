881b5da3d443c39881ee00d61fef0406a619c1cd
Lindos-Mahadi <lindosmahadi@.com>
2021-09-02 09:49:57 +0600

    Toggle with UseState in Functional Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import About from './About';
 
 function Home() {
-    
+    const [toggle, setToggle] = useState();
+
+    const toggler = () =>{
+        toggle ? setToggle() : setToggle(true);
+    }
+
     return (
-        <div>
-            {/* <About text= "Functional About Component" /> */}
-        </div>
+        <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 mx-auto home">
+                        <h1>I am from Home</h1>
+                        <button className="btn btn-info btn-sm" onClick={toggler}>
+                            {toggle? <span>hide</span>:<span>show</span>}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
