ebe080b4c249cdec229d297e596d8b800c17138d
Lindos-Mahadi <lindosmahadi@.com>
2021-09-03 17:41:08 +0600

    Using Ref createRef

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import reactDOM from 'react-dom'
-class About extends Component {
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    constructor() {
+        super();
+        this.userRef = React.createRef();
+    }
+
+    editVal()
+    {
+        console.warn(this.userRef);
+        this.userRef.current.focus();
+        this.userRef.current.value="1000";
+    }
     render() {
         return (
-            reactDOM.createPortal(
-                <h1>Portal Component</h1>,
-                document.getElementById('other-root')
-            )
-        );
+            <div>
+                <h1>I am Ref In React</h1>
+                <input ref={this.userRef} type="text" name="user" />
+                <button onClick={()=>this.editVal()}>Click Me</button>
+            </div>
+        )
     }
 }
-
-export default About;
